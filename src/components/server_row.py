fb548b669107b5e4a4d385ed28c72233ae30405c
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2023-10-02 01:17:43 +0200

    Feature-complete servers view

--- src/components/server_row.py
+++ src/components/server_row.py
@@ -8,12 +8,22 @@ class ServerRow(Adw.ActionRow):
     __gtype_name__ = "MarmaladeServerRow"
 
     button = Gtk.Template.Child()
-    button_content = Gtk.Template.Child()
+    button_revealer = Gtk.Template.Child()
+    tick = Gtk.Template.Child()
+    tick_revealer = Gtk.Template.Child()
     server: Server
+
+    @property
+    def is_selected(self):
+        return self.tick.get_active()
 
     @GObject.Signal(name="button-clicked")
     def button_clicked(self):
         """Signal emitted when the row button is clicked"""
+
+    @GObject.Signal(name="selected-changed", arg_types=[bool])
+    def selected_changed(self, _is_selected: bool):
+        """Signal emitted when the tick is check or unchecked"""
 
     def __init__(
         self,
@@ -23,8 +33,10 @@ class ServerRow(Adw.ActionRow):
     ) -> None:
         super().__init__(**kwargs)
         self.server = server
-        self.button_content.set_icon_name(icon_name)
+        self.parent_selection_change_connection = None
+        self.button.set_icon_name(icon_name)
         self.button.connect("clicked", self.on_button_clicked)
+        self.tick.connect("toggled", self.on_tick_toggled)
         self.update_from_server()
 
     def on_button_clicked(self, _button) -> None:
@@ -33,3 +45,14 @@ class ServerRow(Adw.ActionRow):
     def update_from_server(self) -> None:
         self.set_title(self.server.name)
         self.set_subtitle(self.server.address)
+
+    def toggle_button_visible(self) -> None:
+        is_revealed = self.button_revealer.get_reveal_child()
+        self.button_revealer.set_reveal_child(not is_revealed)
+
+    def toggle_tick_visible(self) -> None:
+        is_revealed = self.tick_revealer.get_reveal_child()
+        self.tick_revealer.set_reveal_child(not is_revealed)
+
+    def on_tick_toggled(self, _tick) -> None:
+        self.emit("selected-changed", self.is_selected)
--- src/components/servers_view.py
+++ src/components/servers_view.py
@@ -31,35 +31,44 @@ from src.server import Server
 class ServersView(Adw.Bin):
     __gtype_name__ = "MarmaladeServersView"
 
-    edit_servers_button = Gtk.Template.Child()
-    add_server_button = Gtk.Template.Child()
-    remove_selected_servers_button = Gtk.Template.Child()
+    edit_button = Gtk.Template.Child()
+    add_button = Gtk.Template.Child()
+    add_button_revealer = Gtk.Template.Child()
+    remove_selected_button = Gtk.Template.Child()
+    remove_selected_button_revealer = Gtk.Template.Child()
     server_rows_group = Gtk.Template.Child()
 
     window: Gtk.Window
     servers: ReactiveSet[Server]
-    server_rows = dict[Server, ServerRow]
+    server_rows_mapping: dict[Server, ServerRow]
     server_add_dialog: Optional[ServerAddDialog] = None
+
+    edit_mode: bool
 
     def __init__(self, window: Gtk.Window, servers: ReactiveSet[Server], **kwargs):
         super().__init__(**kwargs)
-        self.server_rows = {}
+        self.edit_mode = False
+        self.server_rows_mapping = {}
         self.window = window
         self.servers = servers
         for server in self.servers:
             self.create_server_row(server)
         self.servers.emitter.connect("item-added", self.on_server_added)
         self.servers.emitter.connect("item-removed", self.on_server_removed)
-        self.add_server_button.connect("clicked", self.on_add_server_button_clicked)
+        self.add_button.connect("clicked", self.on_add_button_clicked)
+        self.edit_button.connect("clicked", self.on_edit_button_clicked)
+        self.remove_selected_button.connect(
+            "clicked", self.on_remove_selected_button_clicked
+        )
 
     def create_server_row(self, server: Server) -> None:
         row = ServerRow(server)
-        self.server_rows[server] = row
-        self.server_rows_group.add(row)
+        self.server_rows_mapping[server] = row
+        self.server_rows_group.append(row)
 
     def remove_server_row(self, server: Server):
-        row = self.server_rows[server]
-        del self.server_rows[server]
+        row = self.server_rows_mapping[server]
+        del self.server_rows_mapping[server]
         self.server_rows_group.remove(row)
 
     def on_server_added(self, _emitter, server: Server) -> None:
@@ -68,7 +77,7 @@ class ServersView(Adw.Bin):
     def on_server_removed(self, _emitter, server: Server) -> None:
         self.remove_server_row(server)
 
-    def on_add_server_button_clicked(self, _button) -> None:
+    def on_add_button_clicked(self, _button) -> None:
         dialog = ServerAddDialog()
         self.server_add_dialog = dialog
         dialog.connect("server-picked", self.on_add_server_dialog_picked)
@@ -77,3 +86,21 @@ class ServersView(Adw.Bin):
 
     def on_add_server_dialog_picked(self, _dialog, server: Server) -> None:
         self.servers.add(server)
+
+    def toggle_edit_mode(self) -> None:
+        self.edit_mode = not self.edit_mode
+        self.add_button_revealer.set_reveal_child(not self.edit_mode)
+        self.remove_selected_button_revealer.set_reveal_child(self.edit_mode)
+        self.edit_button.set_css_classes(["raised"] if self.edit_mode else ["flat"])
+        for server_row in self.server_rows_mapping.values():
+            server_row.toggle_button_visible()
+            server_row.toggle_tick_visible()
+
+    def on_edit_button_clicked(self, _button) -> None:
+        self.toggle_edit_mode()
+
+    def on_remove_selected_button_clicked(self, _button) -> None:
+        rows = self.server_rows_mapping.values()
+        servers = [row.server for row in rows if row.is_selected]
+        self.servers.difference_update(servers)
+        self.toggle_edit_mode()
