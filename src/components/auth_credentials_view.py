760cb8c35ea94e2683c70e72364bfd2e11dcaa3e
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2023-10-13 21:58:31 +0200

    Focus password row

--- src/components/auth_credentials_view.py
+++ src/components/auth_credentials_view.py
@@ -1,3 +1,5 @@
+import logging
+
 from gi.repository import Adw, GObject, Gtk
 
 from src import build_constants
@@ -10,12 +12,14 @@ from src.database.api import ServerInfo
 class AuthCredentialsView(Adw.NavigationPage):
     __gtype_name__ = "MarmaladeAuthCredentialsView"
 
-    log_in_button = Gtk.Template.Child()
-    username_editable = Gtk.Template.Child()
-    password_editable = Gtk.Template.Child()
+    # fmt: off
+    __log_in_button     = Gtk.Template.Child("log_in_button")
+    __username_editable = Gtk.Template.Child("username_editable")
+    __password_editable = Gtk.Template.Child("password_editable")
+    # fmt: on
 
-    dialog: Adw.Window
-    server: ServerInfo
+    __dialog: Adw.Window
+    __server: ServerInfo
 
     @GObject.Signal(name="authenticated", arg_types=[str])
     def authenticated(self, _user_id: str):
@@ -25,10 +29,13 @@ class AuthCredentialsView(Adw.NavigationPage):
         self, *args, dialog: Adw.Window, server: ServerInfo, username: str, **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
-        self.server = server
-        self.dialog = dialog
-        self.username_editable.set_text(username)
-        # TODO select the password row if username was passed
+        self.__server = server
+        self.__dialog = dialog
+        self.__username_editable.set_text(username)
+
+    def focus_password(self) -> None:
+        """Focus the password row"""
+        self.__password_editable.grab_focus_without_selecting()
 
     def on_log_in_request(self, _widget) -> None:
         # TODO implement logging in with username and password
