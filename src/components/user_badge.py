a9b401349cf550a000e9635a4a42bb1269fb8883
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2024-08-17 22:54:40 +0200

    Removed user badge template
    - Created WidgetBuilder
    - Created widget_factory

--- src/components/user_badge.py
+++ src/components/user_badge.py
@@ -2,10 +2,11 @@ import logging
 from http import HTTPStatus
 from pathlib import Path
 
-from gi.repository import Adw, GObject, Gtk
+from gi.repository import Adw, GObject, Gtk  # type: ignore
 from jellyfin_api_client.errors import UnexpectedStatus
 
-from src import build_constants, shared
+from src import shared
+from src.components.utils import widget_factory
 from src.database.api import ServerInfo, UserInfo
 from src.jellyfin import JellyfinClient
 from src.task import Task
@@ -19,46 +20,75 @@ class NoUserImageError(ImageDownloadError):
     """Error raised when a user has no image"""
 
 
-@Gtk.Template(resource_path=build_constants.PREFIX + "/templates/user_badge.ui")
 class UserBadge(Adw.Bin):
     __gtype_name__ = "MarmaladeUserBadge"
 
-    button = Gtk.Template.Child()
-    avatar = Gtk.Template.Child()
-    label = Gtk.Template.Child()
+    __button: Gtk.Button
+    __avatar: Adw.Avatar
+    __label: Gtk.Label
 
-    image_dir: Path
-    image_path: Path
-    image_size: int
+    __image_dir: Path
+    __image_path: Path
+    __image_size: int
 
-    server: ServerInfo
-    user: UserInfo
+    __server: ServerInfo
+    __user: UserInfo
 
     @GObject.Signal(name="clicked")
-    def user_picked(self):
+    def clicked_signal(self, *_args):
         """Signal emitted when the widget is clicked"""
+
+    def __init_widget(self) -> None:
+        """Create the widget structure"""
+
+        self.__avatar = widget_factory(
+            klass=Adw.Avatar,
+            properties={
+                "icon_name": "avatar-default-symbolic",
+                "valign": Gtk.Align.CENTER,
+                "margin_bottom": 8,
+                "size": 128,
+            },
+        )
+        self.__label = widget_factory(
+            klass=Gtk.Label,
+            properties={
+                "css_classes": ["dim-label", "heading"],
+                "justify": Gtk.Justification.CENTER,
+                "valign": Gtk.Align.CENTER,
+                "wrap": True,
+            },
+        )
+        self.__button = widget_factory(
+            klass=Gtk.Button,
+            properties={"css_classes": ["flat"]},
+            children=widget_factory(
+                klass=Gtk.Box,
+                properties={"orientation": Gtk.Orientation.VERTICAL},
+                children=(self.__avatar, self.__label),
+            ),
+        )
+        self.__button.connect("clicked", self.clicked_signal)
+        self.set_child(self.__button)
 
     def __init__(self, *args, server: ServerInfo, user: UserInfo, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.server = server
-        self.user = user
-        self.image_dir = (
+        self.__init_widget()
+        self.__server = server
+        self.__user = user
+        self.__image_dir = (
             shared.app_cache_dir
             / "servers"
-            / self.server.server_id
+            / self.__server.server_id
             / "users"
-            / self.user.user_id
+            / self.__user.user_id
             / "images"
         )
-        self.image_path = self.image_dir / "profile.png"
-        self.image_size = self.avatar.get_size()
-        self.label.set_label(user.name)
-        self.avatar.set_text(user.name)
-        self.button.connect("clicked", self.on_button_clicked)
+        self.__image_path = self.__image_dir / "profile.png"
+        self.__image_size = self.__avatar.get_size()
+        self.__label.set_label(user.name)
+        self.__avatar.set_text(user.name)
         self.load_image()
-
-    def on_button_clicked(self, _button):
-        self.emit("clicked")
 
     def load_image(self) -> None:
         """
@@ -69,11 +99,13 @@ class UserBadge(Adw.Bin):
         """
 
         def download_image():
-            client = JellyfinClient(base_url=self.server.address, device_id="").get_httpx_client()
-            url = f"/Users/{self.user.user_id}/Images/Profile"
+            client = JellyfinClient(
+                base_url=self.__server.address, device_id=""
+            ).get_httpx_client()
+            url = f"/Users/{self.__user.user_id}/Images/Profile"
             params = {
                 "format": "Png",
-                "width": self.image_size,
+                "width": self.__image_size,
                 # "height": self.image_size,
             }
             response = client.get(url, params=params)
@@ -87,29 +119,29 @@ class UserBadge(Adw.Bin):
                     status_code=response.status_code,
                     content=response.content,
                 )
-            self.image_dir.mkdir(parents=True, exist_ok=True)
-            with self.image_path.open("wb") as file:
+            self.__image_dir.mkdir(parents=True, exist_ok=True)
+            with self.__image_path.open("wb") as file:
                 file.write(response.content)
             return
 
         def on_error(error: Exception):
             match error:
                 case NoUserImageError():
-                    logging.debug("%s has no user image", self.user.name)
+                    logging.debug("%s has no user image", self.__user.name)
                 case _:
                     logging.debug(
                         "Couldn't download %s's profile image",
-                        self.user.name,
+                        self.__user.name,
                         exc_info=error,
                     )
 
         def on_success(_result=None):
-            picture = Gtk.Picture.new_for_filename(str(self.image_path))
+            picture: Gtk.Picture = Gtk.Picture.new_for_filename(str(self.__image_path))  # type: ignore
             paintable = picture.get_paintable()
-            self.avatar.set_custom_image(paintable)
+            self.__avatar.set_custom_image(paintable)
 
         # Use the local image if present
-        if self.image_path.is_file():
+        if self.__image_path.is_file():
             on_success()
             return
 
