5dfb3c9090e43275c758ee33a9e7393a53949d19
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2024-08-22 13:42:23 +0200

    Removed disconnect_dialog template

--- src/components/disconnect_dialog.py
+++ src/components/disconnect_dialog.py
@@ -1,8 +1,23 @@
 from gi.repository import Adw, Gtk
 
-from src import build_constants
 
-
-@Gtk.Template(resource_path=build_constants.PREFIX + "/templates/disconnect_dialog.ui")
 class DisconnectDialog(Adw.MessageDialog):
     __gtype_name__ = "MarmaladeDisconnectDialog"
+
+    def __init_widget(self):
+        self.set_heading(_("Exit Server"))
+        # fmt: off
+        self.set_body(_("Disconnecting without logging out preserves the user session and avoids having to authenticate later."))
+        # fmt: on
+
+        for name, text, appearance in (
+            ("cancel", _("Cancel"), Adw.ResponseAppearance.DEFAULT),
+            ("log-off", _("Disconnect"), Adw.ResponseAppearance.SUGGESTED),
+            ("log-out", _("Log Out"), Adw.ResponseAppearance.DESTRUCTIVE),
+        ):
+            self.add_response(name, text)
+            self.set_response_appearance(name, appearance)
+
+    def __init__(self):
+        super().__init__()
+        self.__init_widget()
